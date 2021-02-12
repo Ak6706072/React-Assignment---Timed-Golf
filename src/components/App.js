c83bcf53ab63077c4b29b0858700262ee6facd6c
Ashish kumar <ak6706072@gmail.com>
2021-02-12 06:06:35 +0530

    timed golf Assignments

--- src/components/App.js
+++ src/components/App.js
@@ -3,22 +3,90 @@ import "../styles/App.css";
 class Timer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0, x: 0, y: 0 };
+    this.id = 0;
+    this.state = { time: 0, x: 0, y: 0,id:0 };
+    this.handleClick =  this.handleClick.bind(this);
+    this.runTimer =  this.runTimer.bind(this);
+    this.ballPosition = {
+          left:this.state.x+"px",
+          top:this.state.y+"px",
+    }
+  }
+
+  handleClick(event){
+    
+    switch(event.keyCode){
+      case 37:
+        //left arrow key
+        // console.log(event.keyCode)
+        this.setState(prevState => {
+          return {...prevState,x:this.state.x-5};
+        })
+        // console.log(this.ballPosition,this.state.x)
+        break;
+      case 38:
+        //top arrow key
+        this.setState(prevState => {
+          return {...prevState,y:this.state.y-5};
+        })
+        break;
+      case 39:
+        //right arrow key;
+        this.setState(prevState => {
+          return {...prevState,x:this.state.x+5};
+        })
+        break;
+      case 40:
+        //down arrow key
+        this.setState(prevState => {
+          return {...prevState,y:this.state.y+5};
+        })
+        break;
+      default:
+        break;
+    }
   }
   componentDidMount() {
     
+    
+  }
+  componentDidUpdate(prevProp,prevState,ss) {
+    // console.log("componentdid update",this.state);
+    if(this.state.x===250&&this.state.y===250){
+      clearInterval(this.id);
+      document.removeEventListener("keydown",this.handleClick);
+    }
   }
 
   componentWillUnmount() {
     
   }
 
+  runTimer(){
+
+    if(!this.id){
+      document.addEventListener("keydown",this.handleClick);
+      this.id = setInterval(()=>{
+        this.setState(prevState => {
+          return {...prevState,time:this.state.time+1};
+        })
+      },1000)
+      // console.log("hello",this.id)
+    }
+  }
 
 
   render() {
     return (
- <>
-</>
+          <>
+            <div style={{
+                    left:this.state.x+"px",
+                    top:this.state.y+"px",
+                 }} className="ball"></div>
+            <button onClick={this.runTimer} style={{position:"absolute",top:400,left:300}} className="start">Start</button>
+            <div className="heading-timer">Timer: {this.state.time}</div>
+            <div className="hole"></div>
+          </>
     );
   }
 }
